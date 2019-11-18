4ebad9c124412e41ed32a41616a3e3a40acf15d4
DavisLin <davislin@trevi.cc>
2019-11-18 21:38:59 +0800

    Create a component and rename folder.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <DateTimePicker></DateTimePicker>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import DateTimePicker from '@/components/DateTimePicker.vue'
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    DateTimePicker
   }
 };
 </script>
---
+++ src/components/DateTimePicker.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    date-time-picker
+    <input class="picker" type="text"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DateTimePicker'
+}
+</script>
+
+<style lang="scss" scoped>
+.picker {
+  border: 1px solid black;
+}
+</style>
